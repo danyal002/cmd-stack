27e182d8fefa2e9766a67ec1421e00a616a91a6c
danyal002 <danyal.ak002@gmail.com>
2024-04-28 16:40:09 -0400

    Barebones implementation of the add command along with Command struct

--- cli/cmd/add.go
+++ cli/cmd/add.go
@@ -1,11 +1,12 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
+	"cmdstack/dal"
+	"log"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -13,28 +14,41 @@ import (
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "missing_docs",
+	Long:  `missing_docs`,
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		cmdText := args[0]
+		tag, _ := cmd.Flags().GetString("tag")
+		note, _ := cmd.Flags().GetString("note")
+		alias, _ := cmd.Flags().GetString("alias")
+
+		command := Command{
+			Command: cmdText,
+			Tag:     tag,
+			Note:    note,
+			Alias:   alias,
+			Id:      nil,
+		}
+
+		command_id := dal.GenerateId()
+		command.Id = &command_id
+
+		command_str, err := command.Serialize()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = dal.Add(strconv.FormatUint(*command.Id, 10), string(command_str))
+		if err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	addCmd.Flags().StringP("tag", "t", "", "Tag for the command")
+	addCmd.Flags().StringP("note", "n", "", "Note for the command")
+	addCmd.Flags().StringP("alias", "a", "", "Alias for the command")
 }
