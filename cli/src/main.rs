17deeeba26881709d7d159d93e7ccb53acc088ff
Pop0097 <dhruvrawat09@gmail.com>
2024-07-11 15:06:47 -0400

    Send logs to home directory

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -10,17 +10,38 @@ mod param;
 
 use args::{CmdStackArgs, Command};
 use clap::Parser;
+use log::LevelFilter;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Config, Root};
 
 fn main() {
-    env_logger::init();
+    // Set up logging. Can log in any crate using the log crate
+    // 
+    // Log file path: $HOME/.config/cmdstack/cmdstack.log    
+    let home_dir = dirs::home_dir().unwrap();
+    let logfile_path = home_dir.to_str().unwrap().to_string() + "/.config/cmdstack/cmdstack.log";
 
-    let args = CmdStackArgs::parse();
+    let logfile = FileAppender::builder()
+        .build(logfile_path).unwrap();
 
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder()
+                   .appender("logfile")
+                   .build(LevelFilter::Info)).unwrap();
+
+    log4rs::init_config(config).unwrap();
+
+    // Configure inquire
     inquire::set_global_render_config(inquire::ui::RenderConfig {
         prompt: inquire::ui::StyleSheet::default().with_fg(inquire::ui::Color::LightBlue),
         ..Default::default()
     });
 
+
+    // Parse command line arguments and execute the command
+    let args = CmdStackArgs::parse();
+    
     match args.command {
         Command::Add(add_args) => command::add_command::handle_add_command(add_args),
         Command::Update(update_args) => command::update_command::handle_update_command(update_args),
