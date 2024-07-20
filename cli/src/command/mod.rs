122dd6652d75e513d2813e0f3d80e8ffc6b435d9
Pop0097 <dhruvrawat09@gmail.com>
2024-07-20 16:05:32 -0400

    Much nicer printing of internal commands

--- cli/src/command/mod.rs
+++ cli/src/command/mod.rs
@@ -5,25 +5,38 @@ pub mod search_command;
 pub mod search_utils;
 pub mod update_command;
 
-use colored::Colorize;
 use data::models::InternalCommand;
+use prettytable::{format, Attr, Cell, Row, Table};
 
 pub fn print_internal_command(internal_command: &InternalCommand) {
-    println!("{} {}", "Command:".bold(), internal_command.command);
-    println!("{} {}", "Alias:".bold(), internal_command.alias);
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_CLEAN);
+
+    table.add_row(Row::new(vec![
+        Cell::new("Command:").with_style(Attr::Italic(true)),
+        Cell::new(&internal_command.command),
+    ]));
+    table.add_row(Row::new(vec![
+        Cell::new("Alias:").with_style(Attr::Italic(true)),
+        Cell::new(&internal_command.alias),
+    ]));
     if let Some(tag) = &internal_command.tag {
-        println!("{} {}", "Tag:".bold(), tag);
+        table.add_row(Row::new(vec![
+            Cell::new("Tag:").with_style(Attr::Italic(true)),
+            Cell::new(tag),
+        ]));
     }
     if let Some(note) = &internal_command.note {
-        println!("{} {}", "Note:".bold(), note);
+        table.add_row(Row::new(vec![
+            Cell::new("Note:").with_style(Attr::Italic(true)),
+            Cell::new(note),
+        ]));
     }
-    println!(
-        "{} {}\n",
-        "Favourite:".bold(),
-        if internal_command.favourite {
-            "YES"
-        } else {
-            "NO"
-        }
-    )
+    let favourite_status = if internal_command.favourite { "YES" } else { "NO" };
+    table.add_row(Row::new(vec![
+        Cell::new("Favourite:").with_style(Attr::Italic(true)),
+        Cell::new(favourite_status),
+    ]));
+
+    table.printstd();
 }
