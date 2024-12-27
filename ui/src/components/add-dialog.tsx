689065849b06df08a7ab15ef8c2731937784dab1
Dylan Wong <d76wong@uwaterloo.ca>
2024-12-27 11:03:16 -0800

    fix nav button styles

--- ui/src/components/add-dialog.tsx
+++ ui/src/components/add-dialog.tsx
@@ -5,16 +5,21 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { AddForm } from './add-form';
-import { Button } from "@/components/ui/button";
-
+import { Button } from '@/components/ui/button';
+import { CirclePlus } from 'lucide-react';
 interface AddDialogProps {}
 
 export function AddDialog({}: AddDialogProps) {
   return (
     <Dialog>
-      <DialogTrigger>
-        <Button>Add Command</Button>
-      </DialogTrigger>
+      <div className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2">
+        <DialogTrigger className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+          <Button variant={'default'} size={'sm'} className="justify-start">
+            <CirclePlus className="mr-2 h-4 w-4" />
+            Add Command
+          </Button>
+        </DialogTrigger>
+      </div>
       <DialogContent>
         <DialogHeader>
           <AddForm />
