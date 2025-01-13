03480fc2eb9a95fa33700db0aa9322113141c2f1
Uzair Ahmad <uzair_a@outlook.com>
2025-01-12 19:09:48 -0500

    remove account switcher, add logo header, stub favourites button, fix favourites tab

---
+++ ui/src/components/cmdStackIcon.tsx
@@ -0,0 +1,16 @@
+export const cmdStackIcon = (
+  <svg
+    width="1024"
+    height="1024"
+    viewBox="0 0 68 68"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0 17.45 62.348.744l3.73 13.922L3.73 31.372z" fill="#7DDCFF" />
+    <path
+      d="m3.747 33.131 64.46 3.378-.755 14.394-64.46-3.378z"
+      fill="#4FAFD1"
+    />
+    <path d="M3.326 53.562h64.547v14.414H3.326z" fill="#7DDCFF" />
+  </svg>
+);
